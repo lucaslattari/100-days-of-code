9e344ec07e32398d3d339acd31606c029e0b324b
Lucas Lattari <lucas.lattari@ifsudestemg.edu.br>
2017-01-27 01:59:28 -0200

    Day 15 of #100DaysOfCode

---
+++ Day15/ExampleDrawingTriangle/Plane.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <algorithm>
+
+class CPlane
+{
+	enum Where {COPLANAR, FRONT, BACK};
+
+public:
+	CPlane(void);
+	CPlane(const glm::vec3& N, const glm::vec3& p);
+	~CPlane(void);
+
+	static CPlane FromPoints(const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& v3);
+	Where Classify(const glm::vec3& p);
+	float GetDistance(const glm::vec3& p);
+
+
+	glm::vec3 N;
+	float d;
+};
+
---
+++ Day15/ExampleDrawingTriangle/main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+
+const int EXIT_SUCCESS = 0;
+const int EXIT_FAILURE = -1;
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#include "shader.h"
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_ANY_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", nullptr, nullptr);
+
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+    if(nullptr == window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+
+    if(GLEW_OK != glewInit())
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+
+        return EXIT_FAILURE;
+    }
+
+    glViewport(0, 0, screenWidth, screenHeight);
+
+    Shader ourShader("core.vs", "core.frag");
+
+    GLfloat vertices[] =
+    {
+        //position              //color
+        -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f, //bottom left
+        0.5f, -0.5f, 0.0f,      0.0f, 1.0f, 0.0f, //bottom right
+        0.0f, 0.5f, 0.0f,       0.0f, 0.0f, 1.0f  //middle top
+    };
+
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) (3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ourShader.Use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
+}
